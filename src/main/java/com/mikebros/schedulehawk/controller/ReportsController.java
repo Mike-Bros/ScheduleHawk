e1b00c566872dbacdc98a62b3ec88ed7884b581a
Mike Bros <mbros@roofmaxx.com>
2022-04-24 20:34:40 -0500

    contact schedules added to reports view

--- src/main/java/com/mikebros/schedulehawk/controller/ReportsController.java
+++ src/main/java/com/mikebros/schedulehawk/controller/ReportsController.java
@@ -2,23 +2,38 @@ package com.mikebros.schedulehawk.controller;
 
 import com.mikebros.schedulehawk.DBConnection;
 import com.mikebros.schedulehawk.ScheduleHawkApplication;
+import com.mikebros.schedulehawk.models.Appointment;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 
 public class ReportsController {
 
-
+    @FXML
+    private GridPane mainGrid;
     @FXML
     private BarChart<String, Number> typeChart;
     @FXML
@@ -41,6 +56,7 @@ public class ReportsController {
 
         setTypeChartData();
         setMonthChartData();
+        addTablesForEachContact();
 
         System.out.println("Finished initializing Reports View");
         System.out.println("......................................................................................\n");
@@ -115,6 +131,93 @@ public class ReportsController {
             }
         }
         return count;
+    }
+
+    private void addTablesForEachContact() throws Exception {
+        String query = "SELECT * FROM contacts;";
+        ResultSet contacts = DBConnection.query(query);
+        while (contacts.next()){
+            System.out.println(contacts.getString("Contact_Name"));
+            createTable(contacts.getString("Contact_Name"), contacts.getString("Contact_ID"));
+        }
+    }
+
+    private void createTable(String contactName, String contactId) throws Exception {
+        VBox vBox = new VBox();
+        vBox.setMaxHeight(200);
+
+        Label tableTitle = new Label("Schedule for " + contactName);
+        tableTitle.setFont(new Font("Arial", 18));
+
+        TableView<Appointment> tableView = new TableView();
+        TableColumn<Appointment, String> appointmentId = new TableColumn<>("Appointment_ID");
+        TableColumn<Appointment, String> title = new TableColumn<>("Title");
+        TableColumn<Appointment, String> type = new TableColumn<>("Type");
+        TableColumn<Appointment, String> description = new TableColumn<>("Description");
+        TableColumn<Appointment, String> startDate = new TableColumn<>("Start Date");
+        TableColumn<Appointment, String> startTime = new TableColumn<>("Start Time");
+        TableColumn<Appointment, String> endDate = new TableColumn<>("End Date");
+        TableColumn<Appointment, String> endTime = new TableColumn<>("End Time");
+        TableColumn<Appointment, String> customerId = new TableColumn<>("Customer_ID");
+
+        tableView.getColumns().add(appointmentId);
+        tableView.getColumns().add(title);
+        tableView.getColumns().add(type);
+        tableView.getColumns().add(description);
+        tableView.getColumns().add(startDate);
+        tableView.getColumns().add(startTime);
+        tableView.getColumns().add(endDate);
+        tableView.getColumns().add(endTime);
+        tableView.getColumns().add(customerId);
+
+        appointmentId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        startDate.setCellValueFactory(new PropertyValueFactory<>("startDate"));
+        startTime.setCellValueFactory(new PropertyValueFactory<>("startTime"));
+        endDate.setCellValueFactory(new PropertyValueFactory<>("endDate"));
+        endTime.setCellValueFactory(new PropertyValueFactory<>("endTime"));
+        title.setCellValueFactory(new PropertyValueFactory<>("title"));
+        description.setCellValueFactory(new PropertyValueFactory<>("description"));
+        type.setCellValueFactory(new PropertyValueFactory<>("type"));
+        customerId.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+        tableView.setItems(createAppointments(contactId));
+
+        vBox.getChildren().add(tableTitle);
+        vBox.getChildren().add(tableView);
+
+        GridPane.setMargin(vBox, new Insets(20, 100, 20, 100));
+        mainGrid.addRow(Integer.parseInt(contactId),vBox);
+    }
+
+    private ObservableList<Appointment> createAppointments(String customerId) throws Exception {
+        ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
+        String query = "SELECT * FROM appointments WHERE Customer_ID = " + customerId;
+        ResultSet appointments = DBConnection.query(query);
+
+        while (appointments.next()){
+            Appointment appt = new Appointment();
+
+            appt.set_id(appointments.getString("Appointment_ID"));
+            appt.set_title(appointments.getString("Title"));
+            appt.set_description(appointments.getString("Description"));
+            appt.set_location(appointments.getString("Location"));
+            appt.set_type(appointments.getString("Type"));
+            appt.set_start(convertFromUTC(appointments.getString("Start")));
+            appt.set_end(convertFromUTC(appointments.getString("End")));
+            appt.set_customerID(appointments.getString("Customer_ID"));
+            appointmentList.add(appt);
+        }
+        return appointmentList;
+    }
+
+    private String convertFromUTC(String dt) {
+        LocalDate localDate = LocalDate.parse(dt.split(" ")[0]);
+        LocalTime localTime = LocalTime.parse(dt.split(" ")[1]);
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDate, localTime, ZoneId.of("UTC"));
+
+        dt = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toString();
+        dt = dt.substring(0, 16); // removes zone info
+        dt = dt.replace("T", " ");
+        return dt;
     }
 
     @FXML
